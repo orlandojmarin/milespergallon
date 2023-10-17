97bbb57319f9c8e51042b0de7c55d65549fa59b8
orlandomarin <orlandomarin@orlandos-air.lan>
2023-10-17 14:58:10 -0400

    Miles Per Gallon

---
+++ src/main/java/com/mycompany/milespergallon/Main.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.milespergallon;
+
+/**
+ *
+ * @author orlandomarin
+ */
+public class Main {
+    public static void main(String[] args) 
+    {
+        //create variables for miles driven and gallons of gas used
+        int milesDriven, gallonsGasUsed;
+        
+        //set values for miles driven and gallons of gas used
+        milesDriven = 400;
+        gallonsGasUsed = 10;
+        
+        //print sentence with my name and miles per gallon calculation
+        System.out.println("Orlando Marin got " + milesDriven/gallonsGasUsed + " miles per gallon on his last road trip!");
+    }
+}
